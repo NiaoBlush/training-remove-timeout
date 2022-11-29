7a8415e81286a5025e23caa78132f2b58bcbe64e
NiaoBlush <303138518@qq.com>
2022-11-29 14:54:58 +0800

    关闭练习题对话框

--- training-remove-timeout.js
+++ training-remove-timeout.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         跳过培训超时对话框
-// @version      0.2
-// @description  培训对话框会影响学习的连贯性，影响学习效率
+// @version      0.3
+// @description  培训对话框会影响学习的连贯性，影响学习效率，影响学习质量
 // @author       You
 // @match        http://tp.1safety.cc/student
 // @match        http://tp.1safety.cc/student/study/*
@@ -18,16 +18,19 @@
     $("body").bind("DOMNodeInserted", function (e) {
         const ele = $(e.target)
         if (ele.prop("outerHTML") == "<div></div>") {
-            //console.log("empty", ele)
             ele.bind("DOMNodeInserted", function (em) {
                 const modal = $(em.target)
-                //console.log("inserted", modal)
-                if (modal.attr("class") != undefined && modal.attr("class").indexOf("ant-modal-confirm") > -1 && modal.html().indexOf("您已经5分钟未操作") > -1) {
-                    console.log("检测到超时对话框");
-                    window.setTimeout(function () {
-                        console.log("点击继续")
-                        modal.find("button.ant-btn-primary").trigger("click")
-                    }, 1000)
+                if (modal.attr("class") != undefined && (modal.attr("class").indexOf("ant-modal-confirm") > -1 || modal.attr("class").indexOf("ant-modal-root") > -1)) {
+                    if (modal.html().indexOf("您已经5分钟未操作") > -1) {
+                        window.setTimeout(function () {
+                            modal.find("button.ant-btn-primary").trigger("click")
+                        }, 1000)
+                    }
+                    if (modal.html().indexOf("练习题") > -1) {
+                        window.setTimeout(function () {
+                            modal.find("button[aria-label='Close']").trigger("click")
+                        }, 1000)
+                    }
                 }
             })
         }
