2ce29ae23fdee1d768b84f2ff2edb870168a2954
Yue Yang <yangyue848@sina.com>
2024-11-11 11:19:47 +0800

    加入醒目提醒

--- training-remove-timeout.js
+++ training-remove-timeout.js
@@ -8,7 +8,7 @@
 // @match        tp.1safety.cc/student/train
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=1safety.cc
 // @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.6.0/jquery.min.js
-// @grant        none
+// @grant        GM_addStyle
 // @namespace https://greasyfork.org/users/440597
 // @downloadURL https://update.greasyfork.org/scripts/455517/%E8%B7%B3%E8%BF%87%E5%9F%B9%E8%AE%AD%E8%B6%85%E6%97%B6%E5%AF%B9%E8%AF%9D%E6%A1%86.user.js
 // @updateURL https://update.greasyfork.org/scripts/455517/%E8%B7%B3%E8%BF%87%E5%9F%B9%E8%AE%AD%E8%B6%85%E6%97%B6%E5%AF%B9%E8%AF%9D%E6%A1%86.meta.js
@@ -18,6 +18,18 @@
     "use strict";
     console.log("loaded");
     console.log($.fn.jquery);
+
+    GM_addStyle(`
+    #rm-timeout_overlay {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100vw;
+      height: 100vh;
+      background-color: rgba(217, 0, 0, 0.3);
+      z-index: 9999;
+    }
+    `);
 
     // 使用 MutationObserver 监测 DOM 变化
     const observer = new MutationObserver(function (mutations) {
@@ -39,10 +51,15 @@
                                         console.log("__ modal inserted");
 
                                         if (modal.html().indexOf("您已经5分钟未操作") > -1) {
+                                            const overlay = document.createElement('div');
+                                            overlay.id = 'rm-timeout_overlay';
+                                            document.body.appendChild(overlay);
+
                                             window.setTimeout(function () {
+                                                document.body.removeChild(overlay);
                                                 modal.find("button.ant-btn-primary").trigger("click");
                                                 console.log("__ triggered");
-                                            }, 1000);
+                                            }, 4000);
                                         }
 
                                         if (modal.html().indexOf("练习题") > -1) {
@@ -56,7 +73,7 @@
                             });
                         });
 
-                        modalObserver.observe(node, { childList: true, subtree: true });
+                        modalObserver.observe(node, {childList: true, subtree: true});
                     }
                 });
             }
@@ -64,6 +81,6 @@
     });
 
     // 开始监测 body 的变化
-    observer.observe(document.body, { childList: true, subtree: true });
+    observer.observe(document.body, {childList: true, subtree: true});
 
 })();
